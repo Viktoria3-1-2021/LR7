6a3c5e6b8e959d67afb6fd9a518af4fdaff06773
Виктория Якубова <s106597@guu.ru>
2021-06-24 23:44:25 +0300

    Lab 7 is done

---
+++ LR7_2/LR7_2/LR7_2.cpp
@@ -0,0 +1,37 @@
+﻿//Напишите и протестируйте функцию, которая принимает в качестве аргументов адреса трех переменных double и помещает наименьшее значение в первую переменную, среднее значение — во вторую, а наибольшее значение — в третью.
+#include <iostream>
+using namespace std;
+void perem(double& x, double& y, double& z) //передача по ссылке(адресу);
+{
+    double max, min, med;
+    min = x;
+    if (y < min)
+        min = y;          //поиск минимума;
+    if (z < min)
+        min = z;
+    max = x;
+    if (y > max)
+        max = y;	//поиск максимума;
+    if (z > max)
+        max = z;
+    if (x != min && x != max)
+        med = x;
+    if (y != min && y != max)	//поиск среднего;
+        med = y;
+    if (z != min && z != max)
+        med = z;
+    x = min;
+    y = med;
+    z = max;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double x, y, z;
+    cout << "ВВедите три числа:" << endl;
+    cin >> x >> y >> z;
+    perem(x, y, z);
+    cout << x << " " << y << " " << z << endl;
+    system("pause");
+    return 0;
+}
